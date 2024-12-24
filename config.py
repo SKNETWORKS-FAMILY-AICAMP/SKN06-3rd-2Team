482a36d3fa099abdbb17a76cb3e8ba8060811e6f
zl-zone <clehflek@gmail.com>
2024-12-24 09:14:49 +0900

    Update config.py
    chunk_overlap을 100으로 수정했습니다

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # split 설정
 chunk_size = 1000
-chunk_overlap = 0
+chunk_overlap = 100
 
 # model 설정
 embedding_name = 'text-embedding-3-small'
 model_name = 'gpt-4o-mini'
 
-# Pinecone 설정
+# Chroma 설정
 collection_name = "bluer_db_openai"
 persist_directory = "vector_store/chroma/bluer_db"
 
