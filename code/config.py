f7dcf65b474eb33ff80a619ce9234ac51cc19c48
haeunbaek99@gmail.com <haeunbaek99@gmail.com>
2024-12-27 07:41:06 -0800

    code 정리

---
+++ code/config.py
@@ -0,0 +1,12 @@
+# split 설정
+chunk_size = 500
+chunk_overlap = 100
+
+# model 설정
+embedding_name = 'text-embedding-3-small'
+model_name = 'gpt-4o-mini'
+
+# Chroma 설정
+collection_name = "bluer_db_openai"
+persist_directory = "vector_store/chroma/bluer_db"
+
